4d4c7e18c6a8e92f5695b21da33e6ae152b935e8
Emmon Moses <oemmon@yahoo.com>
2018-07-27 10:10:53 +0300

    Update LoginAttemptsController.js

--- controllers/LoginAttemptsController.js
+++ controllers/LoginAttemptsController.js
@@ -1,47 +1,113 @@
 var app = angular.module('myApp', ['ngCookies']);
 
-app.controller('LoginAttemptsController', function ($scope, $cookies, $log, $interval, $timeout) {
+app.controller('LoginAttemptsController', function ($scope, $cookies, $log, $interval, $timeout, $filter) {
     var counter = 0;
 
-    $scope.setcookietimer = function(){  
-        mycookie = $cookies.get('key1');    
+
+    $scope.setCookie = function(){
+        mycookie = $cookies.getObject('time');
         if(mycookie){
-            console.log("exists")
-            if(mycookie == 10){
-                $cookies.put('key1',10);
+            console.log("exists");
+            if(mycookie.suspendedTime = 0){
+                $scope.timeobj = {
+                    suspendedTime : 0,
+                    suspendedMinutes : 0,
+                    suspendedSeconds : 0
+                }
+                $cookies.putObject('time',$scope.timeobj);
             }
             else{
-                this.suspension();
+                cookietime = $cookies.getObject('time');
+                cookiesuspendedtime = cookietime.suspendedTime;
+                currenttime = new Date();
+                console.log(cookiesuspendedtime);
+                console.log(currenttime.getTime());
+            var timedifference = (cookiesuspendedtime - currenttime.getTime()) / 1000;
+            console.log(timedifference);
+            var minutedifference = Math.floor(timedifference/60);
+            var seconddifference = Math.floor(timedifference - (minutedifference * 60));
+                if(timedifference < 120){
+                    $scope.timeobj = {
+                        suspendedTime : cookiesuspendedtime,
+                        suspendedMinutes : minutedifference,
+                        suspendedSeconds : seconddifference
+                    }
+                    $cookies.putObject('time',$scope.timeobj);
+                    this.suspension();
+                }
+                else{
+                    $scope.timeobj = {
+                        suspendedTime : 0,
+                        suspendedMinutes : 0,
+                        suspendedSeconds : 0
+                    }
+                    $cookies.putObject('time',$scope.timeobj);
+                }
             }
         }
         else{
-            $cookies.put('key1', 10);
-        } 
-    }
-
-    $scope.countdown = function(key1){
-        if(key1<=0){
-            $scope.popup = false;
-            $scope.newcounter = 10;
-            $interval.cancel(stop);
-            $cookies.put('key1',10);
-            return $scope.newcounter;
-        }
-        else{
-            $scope.newcounter = key1 - 1;
-            return $scope.newcounter;
+            $scope.timeobj = {
+                suspendedTime : 0,
+                suspendedMinutes : 0,
+                suspendedSeconds : 0
+            }
+            $cookies.putObject('time',$scope.timeobj);
         }
     }
-
     $scope.suspension = function(){
         $scope.popup = true;
+        mycookie = $cookies.getObject('time');
+        if(mycookie.suspendedTime == 0){
+            newsuspension = new Date();
+            newsuspension2 = newsuspension.getTime();
+            console.log(newsuspension2);
+            newsuspendedtime = newsuspension2 + 120000;
+            console.log(newsuspendedtime);
+            $scope.timeobj = {
+                suspendedTime : newsuspendedtime,
+                suspendedMinutes :2,
+                suspendedSeconds : 0
+            }
+            $cookies.putObject('time',$scope.timeobj);
+            console.log("suspended");
+        }
+        
         stop = $interval(function () {
-            getMyCookie = $cookies.get('key1');
-            $scope.countdown(getMyCookie);
-            $cookies.put('key1',$scope.newcounter);
+            getMyCookie = $cookies.getObject('time');
+            $scope.suspendedtime = getMyCookie.suspendedTime;
+            $scope.Minute = getMyCookie.suspendedMinutes;
+            $scope.Seconds = getMyCookie.suspendedSeconds;
+            $scope.countdown($scope.Minute, $scope.Seconds);
+            $scope.timeobj = {
+                suspendedTime : $scope.suspendedtime,
+                suspendedMinutes :$scope.Minute,
+                suspendedSeconds : $scope.Seconds
+            }
+            $cookies.putObject('time',$scope.timeobj);
          }, 1000);
     }
   
+
+    $scope.countdown = function (minutes,seconds){
+        if(minutes == 0 && seconds == 0){
+            $interval.cancel(stop);
+            $scope.popup = false;
+            $scope.suspendedtime = 0;
+            $scope.Minute = 2;
+            $scope.Seconds = 0;
+        }
+        else if(minutes == 0 && seconds != 0){
+            $scope.Seconds--;
+        }
+        else if(minutes != 0 && seconds == 0){
+            $scope.Minute--;
+            $scope.Seconds = 59;
+        }
+        else if($scope.minutes != 0 && $scope.seconds != 0){
+            $scope.Seconds--;
+        }
+    }
+    
     $scope.checkAttempts = function () {
         var username = $scope.Username;
         var password = $scope.Password;
@@ -69,7 +135,6 @@ app.controller('LoginAttemptsController', function ($scope, $cookies, $log, $int
         if (counter == 3) {
             counter = 1;
             this.suspension();
-            
         }
     };
 
